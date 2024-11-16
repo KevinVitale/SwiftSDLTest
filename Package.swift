72f4d586a261c28d37a597d5c3849afbb24c0004
Kevin Vitale <kevinvitale@gmail.com>
2024-11-16 18:32:52 -0500

    [SPM] updates SwiftSDL to 'dev'

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
       .macOS(.v10_15)
     ],
     dependencies: [
-      .package(url: "https://github.com/KevinVitale/SwiftSDL.git", from: "0.2.0-alpha.10"),
+      .package(url: "https://github.com/KevinVitale/SwiftSDL.git", branch: "dev"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
